420f01362b96b3b82f63eb2263a2a2e7f57385f7
Ankit Arora <ankitarora121@gmail.com>
2020-11-11 23:10:12 +0530

    minor

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,9 @@
 // } from "./infrastructure/session/generic-session-repository/repo";
 // import * as impl from "./infrastructure/session/generic-session-repository/impl"
 // export { SecureContext, ICredentialProvider, ISessionStore, ISecuritySchemaProvider, impl};
+
+
+import {MinimalSecureContext} from "./application/secure-context";
+
+const msc = new MinimalSecureContext({},{}, {})
+msc.getSession("asd");
