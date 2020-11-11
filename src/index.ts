225f40bae441f6f2e4990e65a464adae115b855b
Ankit Arora <ankitarora121@gmail.com>
2020-11-11 23:17:23 +0530

    better index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,2 @@
-// import {MinimalSecureContext, GenericSecureContext, AbstractSecureContext} from "./application/secure-context";
-// import {
-//     ICredentialProvider,
-//     ISecuritySchemaProvider,
-//     ISessionStore
-// } from "./infrastructure/session/generic-session-repository/repo";
-// import * as impl from "./infrastructure/session/generic-session-repository/impl"
-// export { SecureContext, ICredentialProvider, ISessionStore, ISecuritySchemaProvider, impl};
-
-
-import {MinimalSecureContext} from "./application/secure-context";
-
-const msc = new MinimalSecureContext({},{}, {})
-msc.getSession("asd");
+import * as securecontext from "./application/secure-context"
+export {securecontext}
