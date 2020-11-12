317dc87262bdc2f92b60a7b4305aec195dd68103
Ankit Arora <ankitarora121@gmail.com>
2020-11-12 23:53:49 +0530

    Fix types in application layer - 1

--- src/application/secure-context.ts
+++ src/application/secure-context.ts
@@ -1,39 +1,36 @@
-import {ISessionRepository, Session} from "../domain/session";
-import {
-    ICredentialProvider,
-    GenericSessionRepository, ISecuritySchemaProvider,
-    ISessionStore, IAuthBundleProvider, ISecuritySchema
-} from "../infrastructure/session/generic-session-repository/repo";
+import {ISessionRepository, Session, UniqueId} from "../domain/session";
+import {GenericSessionRepository, ISecuritySchema} from "../infrastructure/session/generic-session-repository/repo";
 import {
     InMemoryAuthBundleProvider,
-    InMemoryCredentialProvider, InMemorySecuritySchemaProvider,
+    InMemoryCredentialProvider,
+    InMemorySecuritySchemaProvider,
     InMemorySessionStore
 } from "../infrastructure/session/generic-session-repository/impl";
 import {IAuthProtocolBundle} from "../domain/auth-protocol";
 
 
-export abstract class AbstractSecureContext {
-    private sessionRepo: ISessionRepository;
-    protected constructor(sessionRepo: ISessionRepository) {
+export abstract class AbstractSecureContext<M,SV,C,AS> {
+    private sessionRepo: ISessionRepository<M,SV,C,AS>;
+    protected constructor(sessionRepo: ISessionRepository<M,SV,C,AS>) {
         this.sessionRepo = sessionRepo;
     }
-    public getSession = async (selfId: string, counterpartyId: string): Promise<Session> => {
+    public getSession = async (selfId: UniqueId, counterpartyId: UniqueId): Promise<Session<M,SV,C,AS>> => {
         return this.sessionRepo.getOrCreate(selfId, counterpartyId);
     }
-    public processIncoming = async (selfId: string, senderId: string, message: unknown): Promise<unknown> => {
-        const session: Session = await this.getSession(selfId, senderId);
+    public processIncoming = async (selfId: UniqueId, senderId: UniqueId, message: M): Promise<M> => {
+        const session = await this.getSession(selfId, senderId);
         return session.processIncoming(message);
     }
-    public processOutgoing = async (selfId: string, recipientId: string, message: unknown): Promise<unknown> => {
-        const session: Session = await this.getSession(selfId, recipientId);
+    public processOutgoing = async (selfId: UniqueId, recipientId: UniqueId, message: M): Promise<M> => {
+        const session = await this.getSession(selfId, recipientId);
         return session.processOutgoing(message);
     }
 }
 
-export class MinimalSecureContext extends AbstractSecureContext {
-    constructor(credentialMapping: Record<string, unknown>,
-                authBundleMapping: Record<string, IAuthProtocolBundle>,
-                securitySchemaMapping: Record<string, ISecuritySchema>) {
+export class MinimalSecureContext<M,SV,C,AS> extends AbstractSecureContext<M,SV,C,AS> {
+    constructor(credentialMapping: Record<UniqueId, C>,
+                authBundleMapping: Record<UniqueId, IAuthProtocolBundle<M,SV,C,AS>>,
+                securitySchemaMapping: Record<UniqueId, ISecuritySchema<AS>>) {
         const sessionStore = new InMemorySessionStore();
         const credentialProvider = new InMemoryCredentialProvider(credentialMapping);
         const securitySchemaProvider = new InMemorySecuritySchemaProvider(securitySchemaMapping);
@@ -43,13 +40,13 @@ export class MinimalSecureContext extends AbstractSecureContext {
     }
 }
 
-export class GenericSecureContext extends AbstractSecureContext {
-    constructor(credentialProvider: ICredentialProvider,
-                sessionStore: ISessionStore,
-                securitySchemaProvider: ISecuritySchemaProvider,
-                authBundleProvider: IAuthBundleProvider) {
-        const sessionRepo = new GenericSessionRepository(credentialProvider, sessionStore, securitySchemaProvider, authBundleProvider);
-        super(sessionRepo);
-    }
-}
+// export class GenericSecureContext extends AbstractSecureContext {
+//     constructor(credentialProvider: ICredentialProvider,
+//                 sessionStore: ISessionStore,
+//                 securitySchemaProvider: ISecuritySchemaProvider,
+//                 authBundleProvider: IAuthBundleProvider) {
+//         const sessionRepo = new GenericSessionRepository(credentialProvider, sessionStore, securitySchemaProvider, authBundleProvider);
+//         super(sessionRepo);
+//     }
+// }
 
