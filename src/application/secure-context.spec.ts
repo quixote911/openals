2a45adc7cb8a75c01864b465dfec16a3a107e3f7
Ankit Arora <ankitarora121@gmail.com>
2020-11-13 13:09:46 +0530

    add test case for generic secure context

--- src/application/secure-context.spec.ts
+++ src/application/secure-context.spec.ts
@@ -1,24 +1,82 @@
-import {MinimalSecureContext} from "./secure-context";
+import {GenericSecureContext, MinimalSecureContext} from "./secure-context";
 import apiKeyAuthBundle from "./../infrastructure/auth-protocol/api-key-auth-bundle"
-describe("Testing Minimal Secure Context", () => {
-    const testCredentialMapping = {
-        "myCoinswitchApp1": {
-            "apiKey": "fookey"
-        }
-    }
-    const authBundleMapping = {
-        "apiKey": apiKeyAuthBundle
-    }
-    const securitySchemaMapping = {
-        "coinswitch.co": {
-            authType: "apiKey",
-            settings: {
-                "name": "x-api-key",
-                "in": "header",
+import {CoreOptions} from "request";
+import {
+    InMemoryAuthBundleProvider,
+    InMemoryCredentialProvider, InMemorySecuritySchemaProvider,
+    InMemorySessionStore
+} from "../infrastructure/session/generic-session-repository/impl";
+import {IAuthBundleProvider} from "../infrastructure/session/generic-session-repository/repo";
+
+describe("Testing  Secure Context", () => {
+    describe("Testing Generic Secure Context", () => {
+        const testCredentialMapping = {
+            "myCoinswitchApp1": {
+                "apiKey": "fookey"
             }
         }
-    }
-    it("instantiates with valid inputs", () => {
-        const msc = new MinimalSecureContext(testCredentialMapping, authBundleMapping, securitySchemaMapping);
+        const authBundleMapping = {
+            "apiKey": apiKeyAuthBundle
+        }
+        const securitySchemaMapping = {
+            "coinswitch.co": {
+                authType: "apiKey",
+                settings: {
+                    "name": "x-api-key",
+                    "in": "header",
+                }
+            }
+        }
+        it("instantiates with valid inputs", async () => {
+
+
+            type SV = void;
+            const sessionStore = new InMemorySessionStore<SV>();
+
+            type C = {apiKey: string}
+            const credentialProvider = new InMemoryCredentialProvider<C>(testCredentialMapping);
+
+
+            type AS = {name: string, in: string}
+            const securitySchemaProvider = new InMemorySecuritySchemaProvider<AS>(securitySchemaMapping);
+
+
+            type M = CoreOptions
+            const authBundleProvider: IAuthBundleProvider<M,SV,C,AS> = new InMemoryAuthBundleProvider(authBundleMapping)
+
+            const secureContext = new GenericSecureContext<M,SV,C,AS>(
+                credentialProvider,
+                sessionStore,
+                securitySchemaProvider,
+                authBundleProvider
+            );
+            const session = await secureContext.getSession("localhost", "coinswitch.co")
+        });
+    });
+    describe("Testing Minimal Secure Context", () => {
+        const testCredentialMapping = {
+            "myCoinswitchApp1": {
+                "apiKey": "fookey"
+            }
+        }
+        const authBundleMapping = {
+            "apiKey": apiKeyAuthBundle
+        }
+        const securitySchemaMapping = {
+            "coinswitch.co": {
+                authType: "apiKey",
+                settings: {
+                    "name": "x-api-key",
+                    "in": "header",
+                }
+            }
+        }
+        it("instantiates with valid inputs", () => {
+            type M = CoreOptions
+            type SV = void;
+            type C = {apiKey: string}
+            type AS = {name: string, in: string}
+            const msc = new MinimalSecureContext<M,SV,C,AS>(testCredentialMapping, authBundleMapping, securitySchemaMapping);
+        });
     });
 });
