7c9a0aec6e7a1a96ffc512eff4701844ba293055
ankitarora <ankitarora121@gmail.com>
2022-01-08 00:46:02 +0530

    changeup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,34 @@
-import {GenericSecureContext} from "./application/secure-context"
-import {GenericSessionRepository} from "./infrastructure/session/generic-session-repository/repo"
+import express from "express";
+import  {JSONRPCServer as JSONRPCServer} from "json-rpc-2.0"
+import * as bodyParser from "body-parser";
 
-export {GenericSecureContext, GenericSessionRepository}
+const app = express();
+const server = new JSONRPCServer();
+
+app.use(bodyParser.json());
+
+
+server.addMethod("echo", ( text ) => text);
+server.addMethod("log", ( message ) => console.log(message));
+
+
+
+app.post("/do", (req, res) => {
+    const jsonRPCRequest = req.body;
+    // server.receive takes a JSON-RPC request and returns a promise of a JSON-RPC response.
+    // Alternatively, you can use server.receiveJSON, which takes JSON string as is (in this case req.body).
+    server.receive(jsonRPCRequest).then((jsonRPCResponse) => {
+        if (jsonRPCResponse) {
+            res.json(jsonRPCResponse);
+        } else {
+            // If response is absent, it was a JSON-RPC notification method.
+            // Respond with no content status (204).
+            res.sendStatus(204);
+        }
+    });
+});
+
+
+app.listen(8081, ()=>{
+    console.log("yolo started at http://127.0.0.1:8081")
+});
